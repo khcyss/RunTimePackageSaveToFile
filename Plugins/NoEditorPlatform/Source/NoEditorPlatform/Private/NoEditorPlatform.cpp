bcecae08a161d7f75a048a4ac0129746597a5e50
tanhanxun <hanxun@guju.com.cn>
2021-05-20 18:50:31 +0800

    提交改动

--- Plugins/NoEditorPlatform/Source/NoEditorPlatform/Private/NoEditorPlatform.cpp
+++ Plugins/NoEditorPlatform/Source/NoEditorPlatform/Private/NoEditorPlatform.cpp
@@ -19,7 +19,8 @@ void FNoEditorPlatformModule::ShutdownModule()
 {
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
-
+	//delete Plftform;
+	//Plftform = nullptr;
 	
 }
 
